05b9ecc5dc9d8b8cab4e878f2a159628cc26d7cf
GUSTAVOHENRIQUE <gustavohenriquebso@gmail.com>
2016-07-07 15:49:28 -0300

    sasa

--- frete/src/test/java/dai/ifma/teste/FreteApplicationTests.java
+++ frete/src/test/java/dai/ifma/teste/FreteApplicationTests.java
@@ -1,6 +1,6 @@
 package dai.ifma.teste;
 
-//import org.junit.runner.RunWith;
+import org.junit.runner.RunWith;
 //import org.junit.runners.JUnit4;
 //import org.springframework.boot.test.SpringApplicationConfiguration;
 //import org.springframework.test.context.web.WebAppConfiguration;
