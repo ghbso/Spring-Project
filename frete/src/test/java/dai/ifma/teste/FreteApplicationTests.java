94e94a7909ab7c00bfefcd7fbc35d82e5ee2b455
GUSTAVOHENRIQUE <gustavohenriquebso@gmail.com>
2016-07-07 15:47:51 -0300

    Teste 03

--- frete/src/test/java/dai/ifma/teste/FreteApplicationTests.java
+++ frete/src/test/java/dai/ifma/teste/FreteApplicationTests.java
@@ -2,13 +2,13 @@ package dai.ifma.teste;
 
 //import org.junit.runner.RunWith;
 //import org.junit.runners.JUnit4;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
 //import org.springframework.test.context.web.WebAppConfiguration;
 
-import dai.ifma.FreteApplication;
+//import dai.ifma.FreteApplication;
 
 //@RunWith(JUnit4.class)
-@SpringApplicationConfiguration(classes = FreteApplication.class)
+//@SpringApplicationConfiguration(classes = FreteApplication.class)
 //@WebAppConfiguration
 public class FreteApplicationTests {
 
