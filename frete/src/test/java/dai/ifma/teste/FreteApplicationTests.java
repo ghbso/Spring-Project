00695d383be0bde3efcb153050ac3c11493f9209
GUSTAVOHENRIQUE <gustavohenriquebso@gmail.com>
2016-07-09 08:26:03 -0300

    Novo Testte Unidade

--- frete/src/test/java/dai/ifma/teste/FreteApplicationTests.java
+++ frete/src/test/java/dai/ifma/teste/FreteApplicationTests.java
@@ -1,11 +1,14 @@
 package dai.ifma.teste;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class FreteApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void test() {
+		assertEquals(1,1);
 	}
 
 }
